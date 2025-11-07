07001afa10a3416d3f9158996c486914ad8a86f8
Abraham casas <abrahamcomservice@gmail.com>
2025-11-07 14:02:44 -0600

    Elimino mi carpeta

--- build/resolvers/type/platform.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const resolversPlatformType = {
-    Platform: {
-        active: (parent) => (parent.active !== false ? true : false),
-    },
-};
-exports.default = resolversPlatformType;
--- build/resolvers/type/product.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const resolversProductType = {
-    Product: {
-        screenshoot: (parent) => parent.shortScreenshots,
-    },
-};
-exports.default = resolversProductType;
